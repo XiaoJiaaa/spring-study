cfcd53e97e6f23d88af856915b9ccbe99804b325
XiaoJiaaa <471303939@qq.com>
2021-12-02 08:57:44 +0800

    cors

--- gateway-api/src/main/java/com/example/gatewayapi/GatewayApiApplication.java
+++ gateway-api/src/main/java/com/example/gatewayapi/GatewayApiApplication.java
@@ -3,6 +3,11 @@ package com.example.gatewayapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.util.pattern.PathPatternParser;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -10,6 +15,18 @@ public class GatewayApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApiApplication.class, args);
+    }
+
+    @Bean
+    public CorsWebFilter corsFilter(){
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsWebFilter(source);
     }
 
 }
