d39eab28945d194d20da1c8bd66b1e3980dfbf99
DarkWood_Jia <DarkWood_Jia@163.com>
2022-11-14 21:51:40 +0800

    config

---
+++ feign-api/src/main/java/trace/TraceConfig.java
@@ -0,0 +1,13 @@
+package trace;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class TraceConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new TraceInterceptor()).addPathPatterns("/**");
+    }
+}
