7d594a18d23381915fa2703f8e67f1af3d171fcc
XiaoJiaaa <471303939@qq.com>
2022-12-03 19:05:08 +0800

    Async

--- feign-api/src/main/java/trace/TraceConfig.java
+++ feign-api/src/main/java/trace/TraceConfig.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class TraceConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new TraceInterceptor()).addPathPatterns("/**");
+        registry.addInterceptor(new TraceInterceptor()).addPathPatterns("/**")
+                .excludePathPatterns("/error");
     }
 }
--- spring-eureka-client1/src/main/java/com/example/eureka/EurekaClient1Application.java
+++ spring-eureka-client1/src/main/java/com/example/eureka/EurekaClient1Application.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableEurekaClient
 @SpringBootApplication
 @EnableFeignClients(basePackages = "api")
+@EnableAsync
 public class EurekaClient1Application {
 
     public static void main(String[] args) {
